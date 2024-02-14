2b16e3414c0c2a65ae0602f1e31316ddc9ba019d
Eva Smyth <106627495+evaSmyth@users.noreply.github.com>
2024-02-14 10:30:24 +0000

    update doneButton to toggle properly

--- main.js
+++ main.js
@@ -1,88 +1,98 @@
-'use strict'
+"use strict";
 
+const input = document.querySelector(".input");
+const list = document.querySelector(".list-wrap");
+const submitButton = document.querySelector(".submit-btn");
+const hiddenMessage = document.querySelector(".hidden-msg");
 
-const input = document.querySelector('.input');
-const list = document.querySelector('.list-wrap')
-const submitButton = document.querySelector('.submit-btn')
-const hiddenMessage = document.querySelector('.hidden-msg');
-
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   loadListFromLocalStorage();
+  input.value = "";
 });
 
-
-function buildItem () {
-  const item = document.createElement('div')
-  item.classList.add('item');
+function buildItem(todo) {
+  const item = document.createElement("div");
+  item.classList.add("item");
   list.appendChild(item);
-  const itemName = document.createElement('h3');
-  itemName.classList.add('item-name');
-  itemName.textContent = input.value;
+
+  const itemName = document.createElement("h3");
+  itemName.classList.add("item-name");
+  itemName.textContent = todo.title;
   item.appendChild(itemName);
-  const buttonWrap = document.createElement('div');
-  buttonWrap.classList.add('btn-wrap')
-  const doneButton = document.createElement('button');
-  doneButton.classList.add('btn')
-  doneButton.textContent = 'done'
-  buttonWrap.appendChild(doneButton)
-  const removeButton = document.createElement('button');
-  removeButton.classList.add('btn')
-  removeButton.textContent = 'remove';
+
+  const buttonWrap = document.createElement("div");
+  buttonWrap.classList.add("btn-wrap");
+
+  const doneButton = document.createElement("button");
+  doneButton.classList.add("btn");
+  doneButton.textContent = "done";
+  buttonWrap.appendChild(doneButton);
+
+  if (todo.isComplete) {
+    itemName.classList.add("line-through");
+  }
+
+  const removeButton = document.createElement("button");
+  removeButton.classList.add("btn");
+  removeButton.textContent = "remove";
   buttonWrap.appendChild(removeButton);
   item.appendChild(buttonWrap);
 
-    doneButton.addEventListener('click', function() {
-      itemName.classList.toggle('line-through');
-      doneButton.classList.toggle('line-through');
+  doneButton.addEventListener("click", function () {
+    itemName.classList.toggle("line-through");
+    const doneButtonInitialText = "done";
+    if (doneButton.textContent === doneButtonInitialText) {
+      doneButton.textContent = "undo";
+    } else {
+      doneButton.textContent = doneButtonInitialText;
+    }
 
-      saveListToLocalStorage();
-    });
-
-    removeButton.addEventListener('click', function() {
-      item.remove();
-      saveListToLocalStorage();
-    })
-};
-
-
-function buildList () {
-  submitButton.addEventListener('click', function(e) {
-    e.preventDefault();
-    if(input.value === '') {
-    hiddenMessage.classList.remove('hidden');
-  } else {
-    buildItem();
-    input.value = '';
-    hiddenMessage.classList.add('hidden');
     saveListToLocalStorage();
+  });
+
+  removeButton.addEventListener("click", function () {
+    item.remove();
+    saveListToLocalStorage();
+  });
+}
+
+function buildList() {
+  submitButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (input.value === "") {
+      hiddenMessage.classList.remove("hidden");
+    } else {
+      buildItem({ title: input.value, isComplete: false });
+      input.value = "";
+      hiddenMessage.classList.add("hidden");
+      saveListToLocalStorage();
     }
   });
-};
+}
 
 function saveListToLocalStorage() {
-  const items = Array.from(document.querySelectorAll('.item-name')).map(item => item.textContent);
-  localStorage.setItem('myList', JSON.stringify(items));
+  const items = Array.from(document.querySelectorAll(".item-name")).map(
+    (item) => {
+      console.log(item);
+      return {
+        title: item.innerHTML,
+        isComplete: item.classList.contains("line-through"),
+      };
+    }
+  );
+  localStorage.setItem("myList", JSON.stringify(items));
 }
 
 function loadListFromLocalStorage() {
-  const storedList = localStorage.getItem('myList');
+  const storedList = localStorage.getItem("myList");
   if (storedList) {
     const items = JSON.parse(storedList);
-    items.forEach(itemText => {
-      input.value = itemText;
-      buildItem();
+    items.forEach((todo) => {
+      //input.value = "lol";
+      console.log(input);
+      buildItem(todo);
     });
   }
 }
+
 buildList();
-
-
-
-
-
-
-
-
-
-
-
